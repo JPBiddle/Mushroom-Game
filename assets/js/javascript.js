dcdea407ced861f2a4ae78a8ac0da06a164a5837
JPBiddle <>
2023-12-10 18:18:48 +0000

    Created array of mushroom icons with js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,13 @@
-//test to link js//
+//Array of mushrooms with two of each icon//
 
-document.body.onload = addElement;
-
-function addElement() {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Hello");
-    newDiv.appendChild(newContent);
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-}
+var mushrooms = ["/assets/images/mushroom_001.png", "/assets/images/mushroom_001.png", "/assets/images/mushroom_002.png", "/assets/images/mushroom_002.png",
+    "/assets/images/mushroom_003.png", "/assets/images/mushroom_003.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_005.png",
+    "/assets/images/mushroom_005.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_007.png", "/assets/images/mushroom_007.png",
+    "/assets/images/mushroom_008.png", "/assets/images/mushroom_008.png"];
+mushrooms.forEach(function (image) {
+    var img = document.createElement('img');
+    img.src = image;
+    img.height = "100";
+    img.width = "100";
+    document.body.appendChild(img);
+});
