949938e43a5a80331576dd47d4eafebb3e5f2827
JPBiddle <>
2023-12-10 18:48:57 +0000

    Added function to randomise array of mushrooms

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,13 @@
 //Array of mushrooms with two of each icon//
 
-var mushrooms = ["/assets/images/mushroom_001.png", "/assets/images/mushroom_001.png", "/assets/images/mushroom_002.png", "/assets/images/mushroom_002.png",
+const mushrooms = ["/assets/images/mushroom_001.png", "/assets/images/mushroom_001.png", "/assets/images/mushroom_002.png", "/assets/images/mushroom_002.png",
     "/assets/images/mushroom_003.png", "/assets/images/mushroom_003.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_005.png",
     "/assets/images/mushroom_005.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_007.png", "/assets/images/mushroom_007.png",
     "/assets/images/mushroom_008.png", "/assets/images/mushroom_008.png"];
+
+mushrooms.sort(function () { return 0.5 - Math.random(); });
+
+
 mushrooms.forEach(function (image) {
     var img = document.createElement('img');
     img.src = image;
@@ -11,3 +15,31 @@ mushrooms.forEach(function (image) {
     img.width = "100";
     document.body.appendChild(img);
 });
+
+// let random_shroom = mushrooms.sort(() => (math.random() > .5) ? 2 : -1);
+
+// for (var i = 0; i < mushrooms.length; i++) {
+// }
+
+
+
+// function shuffleArray(mushrooms) {
+//     const newArray = [...mushrooms];
+
+//     for (let i = newArray.length - 1; i > 0; --i) {
+//         const j = Math.floor(Math.random() * i);
+//         const temp = newArray[i];
+//         newArray[i] = newArray[j];
+//         newArray[j] = temp;
+//     }
+
+//     return newArray;
+// }
+var random_shroom = mushrooms.sort(() => (math.random() > .5) ? 2 : -1);
+
+for (var i = 0; i < mushrooms.length; i++) {
+}
+// let grid = document.createElement('div');
+// grid.className = 'shroom';
+// grid.innerHTML = random_shroom[i];
+// document.querySelector('.game-grid').appendChild(grid);
