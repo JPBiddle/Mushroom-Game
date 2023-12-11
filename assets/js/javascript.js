13da966648ef0632139ce2aa8c68ef0cb9e69008
JPBiddle <>
2023-12-11 01:24:45 +0000

    added function to reset and randomise array of mushrooms

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,45 +1,29 @@
+
+
 //Array of mushrooms with two of each icon//
+function newgame() {
+    var mushrooms = ["/assets/images/mushroom_001.png", "/assets/images/mushroom_001.png", "/assets/images/mushroom_002.png", "/assets/images/mushroom_002.png",
+        "/assets/images/mushroom_003.png", "/assets/images/mushroom_003.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_005.png",
+        "/assets/images/mushroom_005.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_007.png", "/assets/images/mushroom_007.png",
+        "/assets/images/mushroom_008.png", "/assets/images/mushroom_008.png"];
 
-const mushrooms = ["/assets/images/mushroom_001.png", "/assets/images/mushroom_001.png", "/assets/images/mushroom_002.png", "/assets/images/mushroom_002.png",
-    "/assets/images/mushroom_003.png", "/assets/images/mushroom_003.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_004.png", "/assets/images/mushroom_005.png",
-    "/assets/images/mushroom_005.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_006.png", "/assets/images/mushroom_007.png", "/assets/images/mushroom_007.png",
-    "/assets/images/mushroom_008.png", "/assets/images/mushroom_008.png"];
+    //Randomise array of icons//
+    mushrooms.sort(function () { return 0.5 - Math.random(); });
 
-mushrooms.sort(function () { return 0.5 - Math.random(); });
-
-
-mushrooms.forEach(function (image) {
-    var img = document.createElement('img');
-    img.src = image;
-    img.height = "100";
-    img.width = "100";
-    document.body.appendChild(img);
-});
-
-// let random_shroom = mushrooms.sort(() => (math.random() > .5) ? 2 : -1);
-
-// for (var i = 0; i < mushrooms.length; i++) {
-// }
-
-
-
-// function shuffleArray(mushrooms) {
-//     const newArray = [...mushrooms];
-
-//     for (let i = newArray.length - 1; i > 0; --i) {
-//         const j = Math.floor(Math.random() * i);
-//         const temp = newArray[i];
-//         newArray[i] = newArray[j];
-//         newArray[j] = temp;
-//     }
-
-//     return newArray;
-// }
-var random_shroom = mushrooms.sort(() => (math.random() > .5) ? 2 : -1);
-
-for (var i = 0; i < mushrooms.length; i++) {
+    //Push images into body//
+    mushrooms.forEach(function (mushroom) {
+        var img = document.createElement('img');
+        img.src = mushroom;
+        img.height = "100";
+        img.width = "100";
+        img.className = "icon";
+        document.body.appendChild(img);
+    });
 }
-// let grid = document.createElement('div');
-// grid.className = 'shroom';
-// grid.innerHTML = random_shroom[i];
-// document.querySelector('.game-grid').appendChild(grid);
+
+
+function reset() {
+    const icons = document.getElementsByClassName("icon");
+    while (icons.length > 0) icons[0].remove();
+    newgame();
+}
