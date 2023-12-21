133c5ec53749cf6c8158489bf812975798cc4a68
JPBiddle <>
2023-12-21 21:41:05 +0000

    Beautified and formatted code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,13 @@
-//Modal for rules//
+/*Modal for rules*/
+/* Thank you to Youtuber WebDevSimplified for their video guide which assisted me*/
+/* with building my modals - https://www.youtube.com/watch?v=MBaw_6cPmAw */
+
+
+//Variables//
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
+//add event listeners for rules button//
 openModalButtons.forEach((button) => {
     button.addEventListener("click", () => {
         const modal = document.querySelector(button.dataset.modalTarget);
@@ -16,21 +22,16 @@ overlay.addEventListener("click", () => {
     });
 });
 
-// overlay.addEventListener("click", () => {
-//     const modals = document.querySelectorAll(".modal.active");
-//     if (modal == null) return;
-//     modals.forEach((modal) => {
-//         closeModal(modal);
-//     });
-// });
-
+//Close button on modal//
 
 closeModalButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        const modal = button.closest(".modal-rules");
+        const modal = button.closest(".modal-game");
         closeModal(modal);
     });
 });
+
+//Open function for modal//
 
 function openModal(modal) {
     if (modal == null) return;
@@ -38,12 +39,15 @@ function openModal(modal) {
     overlay.classList.add("active");
 }
 
+//Close function for modal//
+
 function closeModal(modal) {
     if (modal == null) return;
     modal.classList.remove("active");
     overlay.classList.remove("active");
 }
 
+//Function to be called when game won//
 
 function congratulations() {
     const congrats = document.querySelector("#modal-congrats");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,46 @@
 //Array of mushrooms with two of each icon//
 function newgame() {
-    var mushrooms = [
-        "/assets/images/mushroom_001.png",
-        "/assets/images/mushroom_001.png",
-        "/assets/images/mushroom_002.png",
-        "/assets/images/mushroom_002.png",
-        "/assets/images/mushroom_003.png",
-        "/assets/images/mushroom_003.png",
-        "/assets/images/mushroom_004.png",
-        "/assets/images/mushroom_004.png",
-        "/assets/images/mushroom_005.png",
-        "/assets/images/mushroom_005.png",
-        "/assets/images/mushroom_006.png",
-        "/assets/images/mushroom_006.png",
-        "/assets/images/mushroom_007.png",
-        "/assets/images/mushroom_007.png",
-        "/assets/images/mushroom_008.png",
-        "/assets/images/mushroom_008.png",
-    ];
+   var mushrooms = [
+      "/assets/images/mushroom_001.png",
+      "/assets/images/mushroom_001.png",
+      "/assets/images/mushroom_002.png",
+      "/assets/images/mushroom_002.png",
+      "/assets/images/mushroom_003.png",
+      "/assets/images/mushroom_003.png",
+      "/assets/images/mushroom_004.png",
+      "/assets/images/mushroom_004.png",
+      "/assets/images/mushroom_005.png",
+      "/assets/images/mushroom_005.png",
+      "/assets/images/mushroom_006.png",
+      "/assets/images/mushroom_006.png",
+      "/assets/images/mushroom_007.png",
+      "/assets/images/mushroom_007.png",
+      "/assets/images/mushroom_008.png",
+      "/assets/images/mushroom_008.png",
+   ];
 
-    //Randomise array of icons//
-    mushrooms.sort(function () {
-        return 0.5 - Math.random();
-    });
+   //Randomise array of icons//
+   mushrooms.sort(function () {
+      return 0.5 - Math.random();
+   });
 
-    //Create div to hold images//
-    const gamegrid = document.createElement("div");
-    gamegrid.className = "gamegrid";
-    document.body.appendChild(gamegrid);
+   //Create div to hold images//
+   const gamegrid = document.createElement("div");
+   gamegrid.className = "gamegrid";
+   document.body.appendChild(gamegrid);
 
-    //Push images into body//
-    mushrooms.forEach(function (mushroom) {
-        const img = document.createElement("img");
-        img.src = mushroom;
-        img.height = "110";
-        img.width = "110";
-        img.className = "icon";
-        img.addEventListener("click", flipCard);
-        img.style.cursor = "pointer";
-        gamegrid.appendChild(img);
-        return;
-    });
+   //Push images into body//
+   mushrooms.forEach(function (mushroom) {
+      const img = document.createElement("img");
+      img.src = mushroom;
+      img.height = "110";
+      img.width = "110";
+      img.className = "icon";
+      img.addEventListener("click", flipCard);
+      img.style.cursor = "pointer";
+      gamegrid.appendChild(img);
+      return;
+   });
 }
 //flip icon when clicked//
 var beenClicked = false;
@@ -49,115 +49,117 @@ var firstClicked = ``;
 var secondClicked = ``;
 
 function flipCard() {
-    if (noThirdClick) return;
-    if (this === firstClicked) return;
-    this.classList.toggle("icon");
-    this.classList.toggle("revealed");
+   if (noThirdClick) return;
+   if (this === firstClicked) return;
+   this.classList.toggle("icon");
+   this.classList.toggle("revealed");
 
-    //Assigning names for first and second clicked cards//
-    if (!beenClicked) {
-        beenClicked = true;
-        firstClicked = this;
-        return;
-    }
-    secondClicked = this;
-    // beenClicked = false;
-    checkForMatch();
-    // return;
+   //Assigning names for first and second clicked cards//
+   if (!beenClicked) {
+      beenClicked = true;
+      firstClicked = this;
+      return;
+   }
+   secondClicked = this;
+   // beenClicked = false;
+   checkForMatch();
+   // return;
 }
 
 //Function to check for a match//
 function checkForMatch() {
-    let firstClickedData = firstClicked.src;
-    let secondClickedData = secondClicked.src;
-    if (firstClickedData === secondClickedData) {
-        noClick();
-    }
-    unreveal();
-    end();
+   let firstClickedData = firstClicked.src;
+   let secondClickedData = secondClicked.src;
+   if (firstClickedData === secondClickedData) {
+      noClick();
+   }
+   unreveal();
+   end();
 
-    //Function to reset / unreveal icons if no match//
-    function unreveal() {
-        noThirdClick = true;
-        setTimeout(() => {
-            if (firstClicked.classList.contains("matched")) {
-                noThirdClick = false;
-                resetBoard();
-            }
-            firstClicked.classList?.toggle("revealed");
-            firstClicked.classList?.toggle("icon");
-            secondClicked.classList?.toggle("revealed");
-            secondClicked.classList?.toggle("icon");
+   //Function to reset / unreveal icons if no match//
+   function unreveal() {
+      noThirdClick = true;
+      setTimeout(() => {
+         if (firstClicked.classList.contains("matched")) {
             noThirdClick = false;
             resetBoard();
-        }, 750);
-    }
+         }
+         firstClicked.classList?.toggle("revealed");
+         firstClicked.classList?.toggle("icon");
+         secondClicked.classList?.toggle("revealed");
+         secondClicked.classList?.toggle("icon");
+         noThirdClick = false;
+         resetBoard();
+      }, 750);
+   }
 }
 
 function resetBoard() {
-    [beenClicked, noThirdClick] = [false, false];
-    [firstClicked, secondClicked] = [``, ``];
+   [beenClicked, noThirdClick] = [false, false];
+   [firstClicked, secondClicked] = [``, ``];
 }
 //Function to remove click after first icon clicked//
 function noClick() {
-    firstClicked.classList.add("matched");
-    firstClicked.classList.remove("icon");
-    firstClicked.classList.remove("revealed");
-    secondClicked.classList.add("matched");
-    secondClicked.classList.remove("icon");
-    secondClicked.classList.remove("revealed");
-    firstClicked.removeEventListener("click", flipCard);
-    secondClicked.removeEventListener("click", flipCard);
+   firstClicked.classList.add("matched");
+   firstClicked.classList.remove("icon");
+   firstClicked.classList.remove("revealed");
+   secondClicked.classList.add("matched");
+   secondClicked.classList.remove("icon");
+   secondClicked.classList.remove("revealed");
+   firstClicked.removeEventListener("click", flipCard);
+   secondClicked.removeEventListener("click", flipCard);
 }
 
 //Remove current array of mushrooms and generates new using newgame function//
 function reset() {
-    const icons = document.getElementsByClassName("gamegrid");
-    while (icons.length > 0) {
-        icons[0].remove();
-    }
-    const iconsmed = document.getElementsByClassName("gamegrid-medium");
-    while (iconsmed.length > 0) {
-        iconsmed[0].remove();
-    }
-    const iconshard = document.getElementsByClassName("gamegrid-hard");
-    while (iconshard.length > 0) {
-        iconshard[0].remove();
-    }
-    newgame();
-    return;
+   const icons = document.getElementsByClassName("gamegrid");
+   while (icons.length > 0) {
+      icons[0].remove();
+   }
+   const iconsmed = document.getElementsByClassName("gamegrid-medium");
+   while (iconsmed.length > 0) {
+      iconsmed[0].remove();
+   }
+   const iconshard = document.getElementsByClassName("gamegrid-hard");
+   while (iconshard.length > 0) {
+      iconshard[0].remove();
+   }
+   newgame();
+   return;
 }
 
-//Alert to signal game complete//
+//Function to call modal to signal game complete//
 function end() {
-    let finishGame = document.querySelectorAll(".icon");
-    console.log(finishGame.length);
-    if (finishGame.length == 0) {
-        setTimeout(function () {
-            congratulations();
-        }, 500);
-        setTimeout(function () {
-            reset();
-        }, 2000);
-    } else {
-        return;
-    }
+   let finishGame = document.querySelectorAll(".icon");
+   console.log(finishGame.length);
+   if (finishGame.length == 0) {
+      setTimeout(function () {
+         congratulations();
+      }, 500);
+      setTimeout(function () {
+         reset();
+      }, 2000);
+   } else {
+      return;
+   }
 }
+
+//Function to remove mushroom image grid when called//
 
 function nonewgame() {
-    const icons = document.getElementsByClassName("gamegrid");
-    while (icons.length > 0) {
-        icons[0].remove();
-    }
-    const iconsmed = document.getElementsByClassName("gamegrid-medium");
-    while (iconsmed.length > 0) {
-        icons[0].remove();
-    }
-    const iconshard = document.getElementsByClassName("gamegrid-hard");
-    while (iconshard.length > 0) {
-        icons[0].remove();
-    }
-    return;
+   const icons = document.getElementsByClassName("gamegrid");
+   while (icons.length > 0) {
+      icons[0].remove();
+   }
+   const iconsmed = document.getElementsByClassName("gamegrid-medium");
+   while (iconsmed.length > 0) {
+      icons[0].remove();
+   }
+   const iconshard = document.getElementsByClassName("gamegrid-hard");
+   while (iconshard.length > 0) {
+      icons[0].remove();
+   }
+   return;
 }
 
 //Game difficulties//
@@ -165,138 +167,138 @@ function nonewgame() {
 //Medium difficulty//
 
 function medium() {
-    reset();
-    nonewgame();
-    var mushroomsmed = [
-        "/assets/images/mushroom_2_001.png",
-        "/assets/images/mushroom_2_001.png",
-        "/assets/images/mushroom_2_002.png",
-        "/assets/images/mushroom_2_002.png",
-        "/assets/images/mushroom_2_003.png",
-        "/assets/images/mushroom_2_003.png",
-        "/assets/images/mushroom_2_004.png",
-        "/assets/images/mushroom_2_004.png",
-        "/assets/images/mushroom_2_005.png",
-        "/assets/images/mushroom_2_005.png",
-        "/assets/images/mushroom_2_006.png",
-        "/assets/images/mushroom_2_006.png",
-        "/assets/images/mushroom_2_007.png",
-        "/assets/images/mushroom_2_007.png",
-        "/assets/images/mushroom_2_008.png",
-        "/assets/images/mushroom_2_008.png",
-        "/assets/images/mushroom_2_009.png",
-        "/assets/images/mushroom_2_009.png",
-        "/assets/images/mushroom_2_010.png",
-        "/assets/images/mushroom_2_010.png",
-        "/assets/images/mushroom_2_011.png",
-        "/assets/images/mushroom_2_011.png",
-        "/assets/images/mushroom_2_012.png",
-        "/assets/images/mushroom_2_012.png",
-    ];
+   reset();
+   nonewgame();
+   var mushroomsmed = [
+      "/assets/images/mushroom_2_001.png",
+      "/assets/images/mushroom_2_001.png",
+      "/assets/images/mushroom_2_002.png",
+      "/assets/images/mushroom_2_002.png",
+      "/assets/images/mushroom_2_003.png",
+      "/assets/images/mushroom_2_003.png",
+      "/assets/images/mushroom_2_004.png",
+      "/assets/images/mushroom_2_004.png",
+      "/assets/images/mushroom_2_005.png",
+      "/assets/images/mushroom_2_005.png",
+      "/assets/images/mushroom_2_006.png",
+      "/assets/images/mushroom_2_006.png",
+      "/assets/images/mushroom_2_007.png",
+      "/assets/images/mushroom_2_007.png",
+      "/assets/images/mushroom_2_008.png",
+      "/assets/images/mushroom_2_008.png",
+      "/assets/images/mushroom_2_009.png",
+      "/assets/images/mushroom_2_009.png",
+      "/assets/images/mushroom_2_010.png",
+      "/assets/images/mushroom_2_010.png",
+      "/assets/images/mushroom_2_011.png",
+      "/assets/images/mushroom_2_011.png",
+      "/assets/images/mushroom_2_012.png",
+      "/assets/images/mushroom_2_012.png",
+   ];
 
-    //Randomise array of icons//
-    mushroomsmed.sort(function () {
-        return 0.5 - Math.random();
-    });
+   //Randomise array of icons//
+   mushroomsmed.sort(function () {
+      return 0.5 - Math.random();
+   });
 
-    //Create div to hold images//
-    const gamegrid = document.createElement("div");
-    gamegrid.className = "gamegrid-medium";
-    document.body.appendChild(gamegrid);
+   //Create div to hold images//
+   const gamegrid = document.createElement("div");
+   gamegrid.className = "gamegrid-medium";
+   document.body.appendChild(gamegrid);
 
-    //Push images into body//
-    mushroomsmed.forEach(function (mushroom) {
-        const img = document.createElement("img");
-        img.src = mushroom;
-        img.height = "110";
-        img.width = "110";
-        img.className = "icon";
-        img.addEventListener("click", flipCard);
-        gamegrid.appendChild(img);
-        return;
-    });
+   //Push images into body//
+   mushroomsmed.forEach(function (mushroom) {
+      const img = document.createElement("img");
+      img.src = mushroom;
+      img.height = "110";
+      img.width = "110";
+      img.className = "icon";
+      img.addEventListener("click", flipCard);
+      gamegrid.appendChild(img);
+      return;
+   });
 }
 
 //Hard Difficulty//
 
 function hard() {
-    reset();
-    nonewgame();
-    var mushroomshard = [
-        "/assets/images/medium_001.png",
-        "/assets/images/medium_001.png",
-        "/assets/images/medium_002.png",
-        "/assets/images/medium_002.png",
-        "/assets/images/medium_003.png",
-        "/assets/images/medium_003.png",
-        "/assets/images/medium_004.png",
-        "/assets/images/medium_004.png",
-        "/assets/images/medium_005.png",
-        "/assets/images/medium_005.png",
-        "/assets/images/medium_006.png",
-        "/assets/images/medium_006.png",
-        "/assets/images/medium_007.png",
-        "/assets/images/medium_007.png",
-        "/assets/images/medium_008.png",
-        "/assets/images/medium_008.png",
-        "/assets/images/medium_009.png",
-        "/assets/images/medium_009.png",
-        "/assets/images/medium_010.png",
-        "/assets/images/medium_010.png",
-        "/assets/images/medium_011.png",
-        "/assets/images/medium_011.png",
-        "/assets/images/medium_012.png",
-        "/assets/images/medium_012.png",
-        "/assets/images/medium_013.png",
-        "/assets/images/medium_013.png",
-        "/assets/images/medium_014.png",
-        "/assets/images/medium_014.png",
-        "/assets/images/medium_015.png",
-        "/assets/images/medium_015.png",
-    ];
+   reset();
+   nonewgame();
+   var mushroomshard = [
+      "/assets/images/medium_001.png",
+      "/assets/images/medium_001.png",
+      "/assets/images/medium_002.png",
+      "/assets/images/medium_002.png",
+      "/assets/images/medium_003.png",
+      "/assets/images/medium_003.png",
+      "/assets/images/medium_004.png",
+      "/assets/images/medium_004.png",
+      "/assets/images/medium_005.png",
+      "/assets/images/medium_005.png",
+      "/assets/images/medium_006.png",
+      "/assets/images/medium_006.png",
+      "/assets/images/medium_007.png",
+      "/assets/images/medium_007.png",
+      "/assets/images/medium_008.png",
+      "/assets/images/medium_008.png",
+      "/assets/images/medium_009.png",
+      "/assets/images/medium_009.png",
+      "/assets/images/medium_010.png",
+      "/assets/images/medium_010.png",
+      "/assets/images/medium_011.png",
+      "/assets/images/medium_011.png",
+      "/assets/images/medium_012.png",
+      "/assets/images/medium_012.png",
+      "/assets/images/medium_013.png",
+      "/assets/images/medium_013.png",
+      "/assets/images/medium_014.png",
+      "/assets/images/medium_014.png",
+      "/assets/images/medium_015.png",
+      "/assets/images/medium_015.png",
+   ];
 
-    //Randomise array of icons//
-    mushroomshard.sort(function () {
-        return 0.5 - Math.random();
-    });
+   //Randomise array of icons//
+   mushroomshard.sort(function () {
+      return 0.5 - Math.random();
+   });
 
-    //Create div to hold images//
-    const gamegrid = document.createElement("div");
-    gamegrid.className = "gamegrid-hard";
-    document.body.appendChild(gamegrid);
+   //Create div to hold images//
+   const gamegrid = document.createElement("div");
+   gamegrid.className = "gamegrid-hard";
+   document.body.appendChild(gamegrid);
 
-    //Push images into body//
-    mushroomshard.forEach(function (mushroom) {
-        const img = document.createElement("img");
-        img.src = mushroom;
-        img.height = "110";
-        img.width = "110";
-        img.className = "icon";
-        img.addEventListener("click", flipCard);
-        gamegrid.appendChild(img);
-        return;
-    });
+   //Push images into body//
+   mushroomshard.forEach(function (mushroom) {
+      const img = document.createElement("img");
+      img.src = mushroom;
+      img.height = "110";
+      img.width = "110";
+      img.className = "icon";
+      img.addEventListener("click", flipCard);
+      gamegrid.appendChild(img);
+      return;
+   });
 }
 
 //Function to change background image//
 function spring() {
-    document.body.style.backgroundImage = 'url("null")';
-    document.body.style.backgroundImage = 'url("/assets/images/spring.webp")';
+   document.body.style.backgroundImage = 'url("null")';
+   document.body.style.backgroundImage = 'url("/assets/images/spring.webp")';
 }
 
 function summer() {
-    document.body.style.backgroundImage = 'url("null")';
-    document.body.style.backgroundImage = 'url("/assets/images/summer.webp")';
+   document.body.style.backgroundImage = 'url("null")';
+   document.body.style.backgroundImage = 'url("/assets/images/summer.webp")';
 }
 
 function autumn() {
-    document.body.style.backgroundImage = 'url("null")';
-    document.body.style.backgroundImage = 'url("/assets/images/autumn.webp")';
+   document.body.style.backgroundImage = 'url("null")';
+   document.body.style.backgroundImage = 'url("/assets/images/autumn.webp")';
 }
 
 function winter() {
-    document.body.style.backgroundImage = 'url("null")';
-    document.body.style.backgroundImage = 'url("/assets/images/winter.webp")';
+   document.body.style.backgroundImage = 'url("null")';
+   document.body.style.backgroundImage = 'url("/assets/images/winter.webp")';
 }
 
 
@@ -305,21 +307,18 @@ function winter() {
 var isMusicPlaying = false;
 
 function myPlay() {
-    isMusicPlaying = true;
-    var audio = document.getElementById("soundtrack");
-    audio.play();
+   isMusicPlaying = true;
+   var audio = document.getElementById("soundtrack");
+   audio.play();
 }
 
 window.onclick = () => {
-    if (isMusicPlaying) return;
-    myPlay();
+   if (isMusicPlaying) return;
+   myPlay();
 };
 //Mute function//
 
 function mute() {
-    var soundtrack = document.getElementById("soundtrack");
-    soundtrack.muted = !soundtrack.muted;
+   var soundtrack = document.getElementById("soundtrack");
+   soundtrack.muted = !soundtrack.muted;
 }
-
-
-
